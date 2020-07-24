8d9ebe0963b9f83fbde65cac62826661cf84a8ca
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-07-24 12:59:11 +0500

    use gatsby-image + adjustements for small devices.

--- src/components/photography/Photos.tsx
+++ src/components/photography/Photos.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
 
 import styled from '@emotion/styled';
-// import Img from 'gatsby-image'
+import Img from 'gatsby-image';
+import { breakpoints } from '../../styles/variables';
 const StyledPhotos = styled.section`
   .photos {
     display: flex;
-    height: 40rem;
+    height: 45rem;
     overflow-x: scroll;
     padding-bottom: var(--gutter-small);
   }
-`;
 
+  .g-image {
+    min-width: 350px;
+
+    @media (max-width: ${breakpoints.medium0}) {
+      min-width: 300px;
+    }
+
+    @media (max-width: ${breakpoints.medium}) {
+      min-width: 280px;
+    }
+
+    @media (max-width: ${breakpoints.small}) {
+      min-width: 80%;
+    }
+
+    .gatsby-image-wrapper {
+      height: 100%;
+      width: 100%;
+    }
+  }
+`;
 
 const Photos = ({ date, title, link, attachments }: any) => (
   <>
@@ -18,7 +39,9 @@ const Photos = ({ date, title, link, attachments }: any) => (
       <p>{new Date(date).getFullYear()}</p>
       <div className="photos">
         {attachments.map((img: any) => (
-          <img src ={img.src} />
+          <div className="g-image">
+            <Img fluid={img} />
+          </div>
         ))}
       </div>
       <h3>{title}</h3>
