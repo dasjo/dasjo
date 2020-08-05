dd9b4506f2a05091c9eaa9fbc0c0315effa2ef2a
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-08-05 08:35:47 +0000

    pager: small updates.

--- src/components/pager.tsx
+++ src/components/pager.tsx
@@ -1,53 +1,63 @@
-// import { Link } from 'gatsby';
+import React from "react";
 
-import React from 'react'
-
-import styled from '@emotion/styled'
-import { Link } from 'gatsby'
+import styled from "@emotion/styled";
+import { Link } from "gatsby";
 
 const StyledPager = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    max-width: var(--container-small-0);
-    margin-top: var(--gutter-large);
+  display: flex;
+  flex-wrap: wrap;
+  max-width: var(--container-small-0);
+  margin-top: var(--gutter-large);
 
-    a {
-        display: block;
-        &:not(:last-of-type) {
-            margin-right: var(--gutter-small);
-            margin-bottom: var(--gutter-small-3);
-        }
+  a {
+    display: block;
+    &:not(:last-of-type) {
+      margin-right: var(--gutter-small);
+      margin-bottom: var(--gutter-small-3);
     }
+  }
 
-   .active {
-       text-decoration: underline;
-   } 
-`
+  .active {
+    cursor: not-allowed;
+    pointer-events: none;
+    color: inherit;
+  }
+`;
 
 const Pager = ({ pageContext }: any) => {
-    const pages = []
+  const pages = [];
 
-    for (let i = 1; i < pageContext.numberOfPages - 1; i++) {
-        pages.push({
-            number: i + 1,
-        })
-    }
-    return (
-        <StyledPager>
-            Page:&nbsp;&nbsp;<Link activeClassName="active" title={'First Page'} to="/writing">First</Link>
-            {pages.map(
-                (p: any, i: number) => ( 
-                <Link activeClassName="active" 
-                    key={i}
-                    title={`Page ${p.number}`}
-                    to={`/writing/${p.number}`}
-                >
-                    {p.number}
-                </Link> )
-            )}
-            <Link activeClassName="active" title={'Last Page'} to={`/writing/${pageContext.numberOfPages}`}>Last</Link>
-        </StyledPager>
-    )
-}
+  for (let i = 1; i < pageContext.numberOfPages; i++) {
+    pages.push({
+      number: i + 1,
+    });
+  }
 
-export default Pager
+  console.log(pageContext, pageContext.pageNumber);
+
+  return (
+    <StyledPager>
+      <Link
+        activeClassName="active"
+        title={"First Page"}
+        to="/writing"
+        tabIndex={pageContext.pageNumber === 0 ? -1 : 0}
+      >
+        1
+      </Link>
+      {pages.map((p: any, i: number) => (
+        <Link
+          activeClassName="active"
+          key={i}
+          title={`Page ${p.number}`}
+          to={`/writing/${p.number}`}
+          tabIndex={p.number - 1 === pageContext.pageNumber ? -1 : 0}
+        >
+          {p.number}
+        </Link>
+      ))}
+    </StyledPager>
+  );
+};
+
+export default Pager;
