23eaa55580d09366502cb70baef92f21fc4f54c8
Josef Kruckenberg <josef.kruckenberg@liip.ch>
2023-11-01 21:32:48 +0100

    Update copyright.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -76,7 +76,7 @@ const Footer = () => (
           </Link>
         </li>
       </ul>
-      <p>&copy; 2020 Josef Kruckenberg.</p>
+      <p>&copy; Josef Kruckenberg.</p>
     </div>
   </StyledFooter>
 );
